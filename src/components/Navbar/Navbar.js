7e6c2bb043bfd3a216b3db5874a5260168c7d0f7
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-02 03:01:59 -0300

    pequeños cambios 3

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -15,31 +15,44 @@ function Navbar() {
 
   return (
 
-  <div className="Navbar">
+    <nav className="Navbar">
 
 
 
 
 
-    <img src={menusvg} onClick={menuDes} id="menusvgId"></img>
+      <img src={menusvg} onClick={menuDes} id="menusvgId"></img>
 
 
-    <img id="logopalabraId" src={logopalabra} />
-    <img id="logoId" className="logoimg" src={logo} />
-    <ul id='listaNavUl'>
-      <li id='itemHome' ><a href="#">Home</a></li>
-      <li><a href="#">Sobre Nosotros</a></li>
-      <li><a href="#">Productos <img className="downflecha" src={downflecha} />
-      </a></li>
-      <li><a href="#">Capacitaciones</a></li>
-      <li id="itemBoumatic"><a href="#">Boumatic</a></li>
-      <li><a href="#">Noticias</a></li>
-      <li><a href="#">Contacto</a></li>
-    </ul>
-    <img id="searchId" className='searchimg' src={search} />
-    <img id="loginId" className='loginimg' src={login} />
-  </div>
-);
+      <img id="logopalabraId" src={logopalabra} />
+      <img id="logoId" className="logoimg" src={logo} />
+      <nav className='NavDef' id="menu">
+        <ul className="listaNav2" id='listaNavUl'>
+          <li id='itemHome' ><a href="#">Home</a></li>
+          <li><a href="#">Empresa</a></li>
+          <li><a href="#">Productos <img className="downflecha" src={downflecha} />
+          </a>
+
+
+            <ul id='listaprodlist'>
+              <li><a href="#">Enlace 2.1</a></li>
+              <li><a href="#">Bombas de Vacio lobulares y a paleta</a></li>
+              <li><a href="#">Enlace 2.3</a></li>
+            </ul>
+
+
+          </li>
+          <li><a href="#">Capacitaciones</a></li>
+          <li id="itemBoumatic"><a href="#">Boumatic</a></li>
+          <li><a href="#">Noticias</a></li>
+          <li><a href="#">Contacto</a></li>
+        </ul>
+        </nav>
+    
+        <img id="searchId" className='searchimg' src={search} />
+        <img id="loginId" className='loginimg' src={login} />
+        </nav>
+  );
 };
 
 function menuDes() {
