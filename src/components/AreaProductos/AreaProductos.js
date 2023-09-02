5e41706ab99476b7acb869e0abec5a5365541d9a
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-02 04:03:58 -0300

    pequeños cambios 9

--- src/components/AreaProductos/AreaProductos.js
+++ src/components/AreaProductos/AreaProductos.js
@@ -7,6 +7,7 @@ import img2 from "./img2.png"
 import img3 from "./img3.png"
 import img4 from "./img4.png"
 import img5 from "./img5.png"
+import img6 from "./img6.png"
 
 const AreaProductos = () => (
   <div className="AreaProductos">
@@ -17,6 +18,7 @@ const AreaProductos = () => (
       <ProductoCard name="Pisos de Goma" img={img3} />
       <ProductoCard name="Productos de Higiene" img={img4} />
       <ProductoCard name="Ventiladores" img={img5} />
+      <ProductoCard name="VER TODOS" img={img6} />
     </div>
     <h3 className='h3verProd'><a href='#'>Ver más...</a></h3>
   </div>
