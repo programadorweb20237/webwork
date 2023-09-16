43965888751669e44c5274b34a4b414eb6e70ee0
Nico Benavidez <testprogramacion2023@outlook.com>
2023-09-16 20:12:43 -0300

    Se mapea productos y quimicos en una  misma pantallla

--- src/components/API/ConsumirApi.js
+++ src/components/API/ConsumirApi.js
@@ -1,14 +1,18 @@
-// Ejemplo de código en React con el componentee renombrado a 'ConsumirApi'
 import React, { useEffect, useState } from 'react';
 import "./ConsumirApi.css"
 
 function ConsumirApi() {
   const [products, setProducts] = useState([]);
+  const [quimicos, setQuimicos] = useState([]);
 
   useEffect(() => {
-    fetch('https://dairy-api-p1w5.onrender.com/api/products')
+    fetch('http://localhost:80/api-routes.php')
       .then((response) => response.json())
-      .then((data) => setProducts(data))
+      .then((data) => {
+        console.log(data);
+        setProducts(data.products);
+        setQuimicos(data.quimicos);
+      })
       .catch((error) => console.error('Error al obtener los datos:', error));
   }, []);
 
@@ -33,6 +37,32 @@ function ConsumirApi() {
               <td>{product.presentation}</td>
               <td>{product.dealerPrice}</td>
               <td>{product.retailPrice}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
+      <h1>Tabla de Químicos</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Código</th>
+            <th>Descripción</th>
+            <th>Presentación</th>
+            <th>Precio Mayorista</th>
+            <th>Precio Minorista</th>
+            <th>Costo por Kilo </th>
+          </tr>
+        </thead>
+        <tbody>
+          {quimicos.map((quimico) => (
+            <tr key={quimico.quimicoId}>
+              <td>{quimico.code}</td>
+              <td>{quimico.description}</td>
+              <td>{quimico.presentation}</td>
+              <td>{quimico.dealerPrice}</td>
+              <td>{quimico.retailPrice}</td>
+              <td>{quimico.costoKilo}</td>
             </tr>
           ))}
         </tbody>
