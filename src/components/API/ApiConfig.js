6652058824707c38e4fe8b90aa05eb5b06b5d5e3
unknown <testprogramacion2023@outlook.com>
2023-09-22 09:16:44 -0300

    Se cambio el css de Nueva Pagina, para que los h2 tengan color negro y no azul, pero ahora mapa y video se caen en version movil(antes pasaba, luego dejo de pasar y ahora que modifique Nueva Pagina volvio a pasar (creo))

--- src/components/API/ApiConfig.js
+++ src/components/API/ApiConfig.js
@@ -4,6 +4,8 @@
 // IP de Maquina Local
 // 192.168.100.40
 
+// IP de Maquina del Trabajo
+// 192.168.1.26
 
 
-export const apiUrl = 'http://localhost:80';
+export const apiUrl = 'http://192.168.1.26:80';
