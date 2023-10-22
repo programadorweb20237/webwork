37a21b1c179a4b3c9b6e28f3559a6b4a215604ec
Nico Benavidez <testprogramacion2023@outlook.com>
2023-10-21 21:40:01 -0300

    Se extraen noticias desde db, pero falta poder editarlas, agregarlas y completar los campos

--- src/components/Noticias/Noticias.js
+++ src/components/Noticias/Noticias.js
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Noticias.css';
 import NoticiasCard from '../NoticiasCard/NoticiasCard';
 import imgReunion from "./reunion.jpg"
+import { apiUrl } from "../API/ApiConfig"
 
 
 // Este es el objeto Usuario que usamos si ya esta logeado el usuario
@@ -10,51 +11,41 @@ import imgReunion from "./reunion.jpg"
 
 
 
-const Noticias = () => (
 
+function Noticias() {
+  const [noticias, setNoticias] = useState([]);
 
+  useEffect(() => {
+    // Realiza una solicitud GET a la API de noticias
+    fetch(`${apiUrl}/api-item-noticias.php`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log('Datos de noticias recibidos:', data); // Agrega este console.log
+        setNoticias(data.noticiasItems);
+      })
+      .catch((error) => console.error('Error al obtener noticias:', error));
+  }, []);
 
+  return (
+    <div className="Noticias" id="areaNoticiastheId">
+      <h2 className="h2Noticias">NOTICIAS</h2>
+      <div className="areaNoticias">
 
-
-  <div className="Noticias" id="areaNoticiastheId">
-    <h2 className='h2Noticias'>NOTICIAS</h2>
-
-
-    <div className='areaNoticias'>
-      <NoticiasCard
-        img={imgReunion}
-        titulo="Reunion de fin de año."
-        descripcion="A finales de año, el equipo de Dairy Solutions se reunió en un ambiente 
-      cordial y festivo para celebrar los logros del año y planificar el futuro. Durante la 
-      reunión, se compartieron reflexiones sobre los desafíos y éxitos del período, se reconoció 
-      el arduo trabajo de todos los empleados y se reafirmaron los valores de la empresa.
-      La reunión fue una oportunidad para fortalecer los lazos entre 
-      los miembros del equipo y motivarlos para enfrentar el futuro con entusiasmo y determinación."
-
-      />
-
-<NoticiasCard
-        img="https://www.ceupe.com.ve/images/easyblog_articles/230/ges_proyec.png"
-        titulo="Cambio de proyectos."
-        descripcion="En Dairy Solutions, siempre estamos en búsqueda de nuevas oportunidades
-         y desafíos. Hoy, nos complace compartir que estamos explorando nuevos proyectos que
-          nos permitirán seguir brindando soluciones innovadoras en línea con nuestra dedicación
-           a la industria lechera. Mantente informado para conocer más sobre nuestras próximas
-            iniciativas."
-
-      />
-
-
-
-
+    
+        {noticias.map((noticia) => (
+          <NoticiasCard
+            key={noticia.id}
+            img={noticia.imagen}
+            titulo={noticia.titulo}
+            descripcion ={noticia.contenido}
+          />
+        ))}
+      </div>
+      <a href="/noticias">Ver más.</a>
     </div>
-
-    <a href='/noticias'>Ver más.</a>
-  </div>
-);
-
-Noticias.propTypes = {};
-
-Noticias.defaultProps = {};
+  );
+}
 
 export default Noticias;
+
+
