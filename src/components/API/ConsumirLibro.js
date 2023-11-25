983e487cc10640cd26bc79ae80389145d9c470de
Programador2023 <testprogramacion2023@outlook.com>
2023-11-25 16:25:39 -0300

    test0003

--- src/components/API/ConsumirLibro.js
+++ src/components/API/ConsumirLibro.js
@@ -3,44 +3,54 @@ import './ConsumirLibro.css';
 import { Viewer, Worker, SpecialZoomLevel } from '@react-pdf-viewer/core';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css'; // Importa el estilo de default-layout
+import '@react-pdf-viewer/page-navigation/lib/styles/index.css'; // Importa el estilo de page-navigation
 import { searchPlugin } from '@react-pdf-viewer/search';
 import '@react-pdf-viewer/search/lib/styles/index.css';
 import { apiUrl } from './ApiConfig';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout'; // Importa defaultLayoutPlugin
 
+import { pageNavigationPlugin } from '@react-pdf-viewer/page-navigation';
+import { DownArrowIcon, NextIcon, PreviousIcon, UpArrowIcon } from '@react-pdf-viewer/page-navigation';
+import { BookmarkIcon, FileIcon, ThumbnailIcon } from '@react-pdf-viewer/default-layout';
+
+import { Icon } from '@react-pdf-viewer/core';
+
+
+
+// Import styles
+import '@react-pdf-viewer/search/lib/styles/index.css';
+
+
+// Import styles
+import '@react-pdf-viewer/page-navigation/lib/styles/index.css';
+
+import { ScrollMode } from '@react-pdf-viewer/core';
+
+
 
 const ConsumirLibro = () => {
     const word = '8507413';
     const [pdfUrl] = useState(`${apiUrl}/get-libro.php`);
-    const [pdfInstance, setPdfInstance] = useState(null);
-    const [searchInstance, setSearchInstance] = useState(null);
+    const searchPluginInstance = searchPlugin();
 
-    useEffect(() => {
-        if (searchInstance && pdfInstance) {
-            searchInstance.on('search', (e) => {
-                const pageIndex = e.match.pageIndex;
-                pdfInstance.current.scrollToPage(pageIndex + 1, SpecialZoomLevel.ActualSize);
-            });
-        }
-    }, [searchInstance, pdfInstance]);
 
-    const onDocumentLoadSuccess = ({ doc }) => {
-        setPdfInstance(doc);
-        if (searchInstance) {
-            searchInstance.jumpToMatch(word, {
-                callback: (match) => {
-                    if (match) {
-                        const pageIndex = match.pageIndex;
-                        pdfInstance.current.scrollToPage(pageIndex + 1, SpecialZoomLevel.ActualSize);
-                    }
-                },
-            });
-        }
-    };
+    const defaultLayoutPluginInstance = defaultLayoutPlugin({
+       
+        sidebarTabs: (defaultTabs) =>
+            defaultTabs.concat({
+                content: <div style={{ textAlign: 'center', width: '100%' }}>Notes are listed here</div>,
+                icon: (
+                    <Icon size={16}>
+                        <path d="M23.5,17a1,1,0,0,1-1,1h-11l-4,4V18h-6a1,1,0,0,1-1-1V3a1,1,0,0,1,1-1h21a1,1,0,0,1,1,1Z" />
+                        <path d="M5.5 12L18.5 12" />
+                        <path d="M5.5 7L18.5 7" />
+                    </Icon>
+                ),
+                title: 'Notes',
+            }),
+    });
+ 
 
-    const onSearchPlugin = (instance) => {
-        setSearchInstance(instance);
-    };
 
     return (
         <div>
@@ -48,20 +58,9 @@ const ConsumirLibro = () => {
             <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
                 <Viewer
                     fileUrl={pdfUrl}
-                    plugins={[
-                        searchPlugin({
-                            keyword: word,
-                        }),
-                        defaultLayoutPlugin({   // Utiliza defaultLayoutPlugin
-                            searchPlugin: {
-                                keyword: word,
-                            },
-                        }),
-                        (instance) => onSearchPlugin(instance),
-                    ]}
-                    onDocumentLoadSuccess={onDocumentLoadSuccess}
-                    ref={(instance) => onDocumentLoadSuccess(instance)}
+                    plugins={[defaultLayoutPluginInstance,searchPluginInstance]}
                 />
+               
             </Worker>
         </div>
     );
