5986cadeb0b6ed8a9e1d196bb0bb106d16525afb
Programador2023 <testprogramacion2023@outlook.com>
2023-11-25 18:04:03 -0300

    Intentado arreglar pdf

--- src/components/API/ConsumirLibro.js
+++ src/components/API/ConsumirLibro.js
@@ -1,36 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './ConsumirLibro.css';
-import { Viewer, Worker, SpecialZoomLevel } from '@react-pdf-viewer/core';
-import '@react-pdf-viewer/core/lib/styles/index.css';
-
-import '@react-pdf-viewer/search/lib/styles/index.css';
 import { apiUrl } from './ApiConfig';
 
-// Import styles
-import '@react-pdf-viewer/search/lib/styles/index.css';
-
-
+import { Viewer, Worker } from '@react-pdf-viewer/core';
+import { ScrollMode, SpecialZoomLevel } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 
 // Import styles
+import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+
+
+
+
 
 const ConsumirLibro = () => {
     const word = '8507413';
     const [pdfUrl] = useState(`${apiUrl}/get-libro.php`);
-    const defaultLayoutPluginInstance = defaultLayoutPlugin(
-        
-    );
+
+    
+
+    const defaultLayoutPluginInstance = defaultLayoutPlugin();
 
 
 
     return (
         <div>
-            <h1>Productos</h1>
-            <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
+            <h1 >Productos</h1>
+            <Worker  workerUrl="https://unpkg.com/pdfjs-dist@3.4.120/build/pdf.worker.min.js">
                 <Viewer
                     fileUrl={pdfUrl}
                     plugins={[defaultLayoutPluginInstance]}
+                    scrollMode={ScrollMode.Horizontal}
+                    defaultScale={SpecialZoomLevel.PageFit}
+               
               
                 />
                
@@ -40,3 +43,6 @@ const ConsumirLibro = () => {
 };
 
 export default ConsumirLibro;
+
+
+
